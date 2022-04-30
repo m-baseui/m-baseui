830a1057317624e928f3d1d24684bb05f357d450
dashi <297901039@qq.com>
2022-04-30 12:34:38 +0800

    ww

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ const { path } = require('@vuepress/utils')
 module.exports = {
   title: 'm-baseui',
   description: 'm-baseui Component library with Vue3',
-  base: '/', // 这是部署到github相关的配置
+  base: '/baseui/', // 这是部署到github相关的配置
   markdown: {
     code: {
       lineNumbers: false // 代码块显示行号
